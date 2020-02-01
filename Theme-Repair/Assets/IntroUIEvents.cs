cadee44502a8f8ff6ae4f7eafeb643601532c071
Phinixx14 <kendal.myers@hotmail.com>
2020-02-01 15:53:03 -0600

    UI scenes and build settings

---
+++ Theme-Repair/Assets/IntroUIEvents.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroUIEvents : MonoBehaviour
+{
+    public Text msgBox;
+    public Button nextBtn;
+
+    public List<string> messages;
+    public int currentPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (messages.Count == 0) //skip if no intro
+        {
+            SceneManager.LoadScene("Asteroid Field", new LoadSceneParameters(LoadSceneMode.Single));
+            return;
+        }
+
+        nextBtn.Select();
+        currentPage = 0;
+        nextBtn.onClick.AddListener(GoNext);
+        msgBox.text = messages[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GoNext()
+    {
+        int nextPage = currentPage + 1;
+
+        if (nextPage > messages.Count - 1)
+        {
+            SceneManager.LoadScene("Asteroid Field", new LoadSceneParameters(LoadSceneMode.Single));
+        }
+
+        if (nextPage == messages.Count - 1)
+        {
+            nextBtn.GetComponentInChildren<Text>().text = "Launch ->";
+        }
+
+        msgBox.text = messages[nextPage];
+
+        currentPage = nextPage;
+    }
+}
---
+++ Theme-Repair/Assets/UIEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIEvents : MonoBehaviour
+{
+    public Button startBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startBtn.onClick.AddListener(StartGame);
+    }
+
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Intro", new LoadSceneParameters(LoadSceneMode.Single));
+    }
+    
+}
