c3a3415e67e33dc032b87530dda4ce7c6b81e3c2
JustLoren <loren.naegle@gmail.com>
2020-02-01 13:20:03 -0600

    Added a bunch of free assets. Woo free shit!

--- Theme-Repair/Assets/Scripts/Ship/BreakOffPieces.cs
+++ Theme-Repair/Assets/Scripts/Ship/BreakOffPieces.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BreakOffPieces : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Asteroid"))
+        {
+            Debug.Log("A piece breaks off!");
+        }
     }
 }
