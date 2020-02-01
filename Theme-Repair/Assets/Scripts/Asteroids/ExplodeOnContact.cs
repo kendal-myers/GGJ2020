7330ce34bd41d1f59f3cce0d4db7b2c3db79cb12
JustLoren <loren.naegle@gmail.com>
2020-02-01 01:18:07 -0600

    First round of asteroids exploding

---
+++ Theme-Repair/Assets/Scripts/Asteroids/ExplodeOnContact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnContact : MonoBehaviour
+{
+    public GameObject explodedPrefab;
+    public float explosionForce;
+    public float explosionRange;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody.CompareTag("Player"))
+        {
+            Explode(collision.rigidbody.velocity);
+        }
+    }
+
+    public void Explode(Vector3 collisionVelocity)
+    {
+        Destroy(this.GetComponent<Rigidbody>());
+
+        var obj = Instantiate(explodedPrefab, this.transform.position, this.transform.rotation, this.transform.parent);
+        foreach (var rb in obj.GetComponentsInChildren<Rigidbody>())
+        {
+            rb.AddForce(collisionVelocity, ForceMode.VelocityChange);
+            rb.AddExplosionForce(explosionForce, this.transform.position, explosionRange);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
