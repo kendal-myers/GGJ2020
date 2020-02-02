c804313f83299acc183b8fef06cefda280065e33
JustLoren <loren.naegle@gmail.com>
2020-02-02 12:48:20 -0600

    You can now win the game. huzzah.

--- Theme-Repair/Assets/Scripts/Shop/ShopEntry.cs
+++ Theme-Repair/Assets/Scripts/Shop/ShopEntry.cs
@@ -6,12 +6,16 @@ public class ShopEntry : MonoBehaviour
 {
     public int shopId;
     private bool entered = false;
+    public bool isEndOfGame = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.isTrigger && !entered)
         {
             entered = true;
+            if (isEndOfGame)
+                throw new UnityException("YOU WIN! GAME OVER!");
+
             var shop = GameObject.FindObjectOfType<ShopLoader>();
             shop.LoadShop(shopId);
         }
