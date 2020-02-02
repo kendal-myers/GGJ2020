b44e9120b0b3a212ecf8e03cfde1b0d4df1449f1
Phinixx14 <kendal.myers@hotmail.com>
2020-02-01 18:43:58 -0600

    intro update

--- Theme-Repair/Assets/IntroUIEvents.cs
+++ Theme-Repair/Assets/IntroUIEvents.cs
@@ -3,28 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor.UI;
 
 public class IntroUIEvents : MonoBehaviour
 {
     public Text msgBox;
+    public Image imageBox;
     public Button nextBtn;
+    
+    public List<PicNText> pages;
 
-    public List<string> messages;
-    public int currentPage;
+    private int currentPage;
+    private Text nextText;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (messages.Count == 0) //skip if no intro
+        if (pages.Count == 0) //skip if no intro
         {
             SceneManager.LoadScene("Asteroid Field", new LoadSceneParameters(LoadSceneMode.Single));
             return;
         }
 
         nextBtn.Select();
+        nextText = nextBtn.GetComponentInChildren<Text>();
         currentPage = 0;
-        nextBtn.onClick.AddListener(GoNext);
-        msgBox.text = messages[0];
+        msgBox.text = pages[0].text;
+        imageBox.sprite = pages[0].image;
+        nextText.text = pages[0].nextText;
     }
 
     // Update is called once per frame
@@ -33,23 +39,28 @@ public class IntroUIEvents : MonoBehaviour
         
     }
 
-    void GoNext()
+    public void GoNext()
     {
         int nextPage = currentPage + 1;
 
-        if (nextPage > messages.Count - 1)
+        if (nextPage > pages.Count - 1)
         {
             SceneManager.LoadScene("Asteroid Field", new LoadSceneParameters(LoadSceneMode.Single));
             return;
         }
 
-        if (nextPage == messages.Count - 1)
-        {
-            nextBtn.GetComponentInChildren<Text>().text = "Launch ->";
-        }
-
-        msgBox.text = messages[nextPage];
+        msgBox.text = pages[nextPage].text;
+        imageBox.sprite = pages[nextPage].image;
+        nextText.text = pages[nextPage].nextText;
 
         currentPage = nextPage;
+    }
+
+    [System.Serializable]  
+    public class PicNText
+    {
+        public string text;
+        public Sprite image;
+        public string nextText;
     }
 }
